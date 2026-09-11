aa081b49f84e79e24a924cc4d187001729946545
lbe <berland.loic@yahoo.fr>
2026-09-11 11:52:04 +0200

    reduce AH time recive money

--- src/game/AuctionHouse/AuctionHouseMgr.cpp
+++ src/game/AuctionHouse/AuctionHouseMgr.cpp
@@ -178,7 +178,7 @@ void AuctionHouseMgr::SendAuctionSalePendingMail(AuctionEntry* auction)
         std::ostringstream msgAuctionSalePendingBody;
         uint32 auctionCut = auction->GetAuctionCut();
 
-        time_t distrTime = time(nullptr) + HOUR;
+        time_t distrTime = time(nullptr);
 
         msgAuctionSalePendingBody.width(16);
         msgAuctionSalePendingBody << std::right << std::hex << auction->bidder;
@@ -229,7 +229,7 @@ void AuctionHouseMgr::SendAuctionSuccessfulMail(AuctionEntry* auction)
 
         MailDraft(msgAuctionSuccessfulSubject.str(), auctionSuccessfulBody.str())
         .SetMoney(profit)
-        .SendMailTo(MailReceiver(owner, owner_guid), auction, MAIL_CHECK_MASK_COPIED, HOUR);
+        .SendMailTo(MailReceiver(owner, owner_guid), auction, MAIL_CHECK_MASK_COPIED, 0);
     }
 }
 
